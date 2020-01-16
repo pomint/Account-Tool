30493b753645bf146d80924dc107cb202e694952
pomint <59945603+pomint@users.noreply.github.com>
2020-01-16 13:17:10 +0800

    Create launch.ps1

---
+++ launch.ps1
@@ -0,0 +1,4 @@
+$adminUser = "mytest\Administrator"
+$adminPassword = ConvertTo-SecureString "!QAZ2wsx#EDC" -AsPlainText -Force
+$mycreds = New-Object System.Management.Automation.PSCredential -ArgumentList $AdminUser, $AdminPassword
+Start-Process -FilePath "C:\Tool\Account Tool 2.1.exe" -Credential $mycreds -WorkingDirectory "C:\Tool\"
